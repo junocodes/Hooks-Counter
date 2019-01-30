2494ae5b651abfe3d8a86e59d02154a4cad9822b
Justin Juno <hello@juno.codes>
2019-01-30 17:19:18 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <AppWrapper>
+      <AppContent>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+      </AppContent>
+    </AppWrapper>
+  );
+};
 
 export default App;
+
+// Styled Components
+
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const AppContent = styled.div`
+  h1 {
+    font-size: 100px;
+    text-align: center;
+    margin: 0 0 25px 0;
+  }
+  button {
+    border: none;
+    border-radius: 5px;
+    background: black;
+    color: white;
+    font-size: 1em;
+    font-weight: bold;
+    padding: 25px;
+    margin: 5px;
+  }
+`;
